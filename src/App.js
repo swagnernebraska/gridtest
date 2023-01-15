fb2c375256c82ae2ae81fbbcb6e42a966bbcce1c
Scott Wagner <Scott.Wagner@golf.tgw>
2023-01-15 16:44:37 -0600

    Added initial action

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import './App.css';
 
 import GridBoard from './components/GridBoard'
@@ -9,16 +12,18 @@ import MessagePopup from './components/MessagePopup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Grid Testing</h1>
-      </header>
-      <GridBoard />
-      <NextBlock />
-      <ScoreBoard />
-      <Controls />
-      <MessagePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Grid Testing</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <MessagePopup />
+      </div>
+    </Provider>
   );
 }
 
