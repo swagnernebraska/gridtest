efd380d0798b530623b71c2aeb007169415e9ecf
Scott Wagner <Scott.Wagner@golf.tgw>
2023-01-15 17:14:39 -0600

    added connection

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import GridSquare from './GridSquare'
+import { useSelector } from 'react-redux'
+import { shapes } from '../utils/index.js'
 
 // Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
-
-    const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    const nextShape = useSelector((state) => state.game.nextShape)
+    const box = shapes[nextShape][0] // Get the first rotation
+    //const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
     // Map the block to the grid
     const grid = box.map((rowArray, row) => {
         return rowArray.map((square, col) => {
