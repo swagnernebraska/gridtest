e36dbf48ad0bb8e3934cba36624ba068c08945f1
Scott Wagner <Scott.Wagner@golf.tgw>
2023-01-14 22:49:10 -0600

    Initial project version

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+import GridBoard from './components/GridBoard'
+import NextBlock from './components/NextBlock'
+import ScoreBoard from './components/ScoreBoard'
+import Controls from './components/Controls'
+import MessagePopup from './components/MessagePopup'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Grid Testing</h1>
+      </header>
+      <GridBoard />
+      <NextBlock />
+      <ScoreBoard />
+      <Controls />
+      <MessagePopup />
+    </div>
+  );
+}
+
+export default App;
